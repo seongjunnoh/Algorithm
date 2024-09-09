d70895797ae225e57041b12321a8c3a053773b43
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2024-09-09 14:41:14 +0900

    [Gold IV] Title: 빙산, Time: 684 ms, Memory: 181692 KB -BaekjoonHub

---
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -0,0 +1,146 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static class Pair {
+        int x;
+        int y;
+
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static int n, m;
+    static int[][] board;
+    static int[][] temp;
+    static Queue<Pair> queue;
+    static int[][] pos = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    static boolean checkBingsan(int x, int y) {
+        // (x,y) 와 인접한 빙산을 모두 방문 처리
+        // -> 빙산 중 방문 처리가 안된 빙산이 없으면 true, 있으면 false return
+        Queue<Pair> bingsan = new LinkedList<>();
+        boolean[][] visited = new boolean[n][m];
+        bingsan.add(new Pair(x, y));
+        visited[x][y] = true;
+
+        while (!bingsan.isEmpty()) {
+            Pair cur = bingsan.poll();
+            for (int i = 0; i < 4; i++) {
+                int nX = cur.x + pos[i][0];
+                int nY = cur.y + pos[i][1];
+                if (board[nX][nY] > 0 && !visited[nX][nY]) {
+                    visited[nX][nY] = true;
+                    bingsan.add(new Pair(nX, nY));
+                }
+            }
+        }
+
+        // 빙산들 방문 처리 확인
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] > 0 && !visited[i][j]) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        temp = new int[n][m];
+        queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if (board[i][j] > 0) {
+                    queue.add(new Pair(i, j));          // 현재 빙산인 부분 add
+                }
+            }
+        }
+
+        int year = 1;
+        while (true) {
+            // 0이 정답인 경우
+            boolean isThereBingsan = false;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (board[i][j] > 0) isThereBingsan = true;
+                }
+            }
+
+            // 빙산이 하나도 없을때까지 while 문 탈출 X -> 0이 정답
+            if (!isThereBingsan) {
+                bw.write("0");
+                bw.flush();
+                bw.close();
+                br.close();
+                return;
+            }
+
+            while (!queue.isEmpty()) {
+                Pair cur = queue.poll();        // 현재 빙산 위치
+                int count = 0;                  // 현재 빙산 위치와 맞닿아있는 바다 수
+                for (int i = 0; i < 4; i++) {
+                    int nX = cur.x + pos[i][0];
+                    int nY = cur.y + pos[i][1];
+                    if (board[nX][nY] == 0) count++;
+                }
+
+                // 빙산 높이 update
+                temp[cur.x][cur.y] = board[cur.x][cur.y] - count;
+                if (temp[cur.x][cur.y] < 0) temp[cur.x][cur.y] = 0;
+            }
+
+            // temp의 빙산을 board로 옮김
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    board[i][j] = temp[i][j];
+                    if (board[i][j] > 0) {
+                        queue.add(new Pair(i, j));          // 1년 후 빙산인 부분 queue에 add
+                    }
+                }
+            }
+            
+            // 0이 정답인 경우
+            isThereBingsan = false;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (board[i][j] > 0) isThereBingsan = true;
+                }
+            }
+
+            // 빙산이 하나도 없을때까지 while 문 탈출 X -> 0이 정답
+            if (!isThereBingsan) {
+                bw.write("0");
+                bw.flush();
+                bw.close();
+                br.close();
+                return;
+            }
+            
+            // board의 빙산이 분리된 경우 체크
+            if (!checkBingsan(queue.peek().x, queue.peek().y)) {
+                bw.write(String.valueOf(year));
+                bw.flush();
+                bw.close();
+                br.close();
+                return;
+            }
+
+            year++;
+        }
+    }
+}

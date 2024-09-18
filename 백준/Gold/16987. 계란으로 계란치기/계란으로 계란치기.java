aa872cd59a9fa5da1c8401e7fd2b207d8dfa0270
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2024-09-19 00:02:49 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 168 ms, Memory: 15096 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,79 +1,70 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-class Main {
-	
-	static ArrayList<Pair> arr;
-	static int n;
-	static int max;
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		n = Integer.parseInt( bf.readLine() ); // 계란 개수
+public class Main {
 
-		arr = new ArrayList<>(); 
-		max = Integer.MIN_VALUE; // 깨진 계란 수 최대값 찾기
-		
-		for(int i = 0; i < n ; i++) {
-			String[] str = bf.readLine().split(" ");
-			
-			int s = Integer.parseInt( str[0] );
-			int w = Integer.parseInt( str[1] );
-			
-			arr.add(new Pair(s, w));
-		} // 데이터 입력
-		
-		find(0, 0); // 0번째 계란, 0개 깨짐
-		
-		bw.write(max + "\n");
-		
-		bw.flush();
-		bw.close();
+    static int n;
+    static int max = 0;
 
-	}
-	
-	public static class Pair {
-		int s;
-		int w;
-		
-		Pair(int s, int w) {
-			this.s = s;
-			this.w = w;
-		}
-	}
-	
-	public static void find(int cur, int cnt) {
-		
-		if(max < cnt) max = cnt; // 최대값 갱신
-	
-		if(cur == n) return; // 이전에 고른 계란이 마지막 계란이었으면 반환
-		if(arr.get(cur).s <= 0) find(cur + 1, cnt); 
-      	// 이번 계란이 (이미) 깨졌으면 오른쪽(다음) 계란
-		else {
-			for(int i = 0; i < n; i++) {
-				if(cur == i || arr.get(i).s <= 0) continue;
-        		// 같은 계란을 깰 수 없음 + 이미 깨진 계란이면 패스
-				
-				arr.get(cur).s -= arr.get(i).w;
-				arr.get(i).s -= arr.get(cur).w;
-        		// 계란으로 계란치기 시도
-				
-				int new_cnt = cnt; // 깨진 계란 세기 변수
-        		// 따로 두는 이유는 다음 계란 깨기를 위해서.
-				
-				if(arr.get(cur).s <= 0) new_cnt++;
-				if(arr.get(i).s <= 0) new_cnt++;
-        		// 깨진 계란이 있다면 개수 증가
-				
-				find(cur + 1, new_cnt); // 오른쪽(다음) 계란 들기 
-				
-				arr.get(cur).s += arr.get(i).w;
-				arr.get(i).s += arr.get(cur).w;
-        		// 다음 계란 치기를 위해 계란 복구
-			}
-		}
+    static void dfs(int index, int broken, int[] s, int[] w) {
+        if (index == n) {
+            max = Math.max(max, broken);
+            return;
+        }
 
-	}
+        // index 번째의 계란이 이미 깨진 상태이면 다음 계란으로 dfs
+        if (s[index] <= 0) {
+            dfs(index + 1, broken, s, w);
+            return;
+        }
 
+        // index 위치의 계란으로 어떤 계란을 칠 지 정하기
+        for (int i=0; i<n; i++) {
+            // i 번째 계란이 이미 깨진 상태이면 다음 반복문으로
+            if (i == index || s[i] <= 0) continue;
+
+            // i 번째 계란을 치기로 결정
+            s[index] -= w[i];
+            s[i] -= w[index];
+
+            int newBroken = broken;            // broken 값 보존하기 위해 local 변수 도입
+
+            if (s[index] <= 0) newBroken++;
+            if (s[i] <= 0) newBroken++;
+
+            // i 번째 계란으로 다시 dfs
+            dfs(index + 1, newBroken, s, w);
+
+            // s, w 원상 복구
+            s[index] += w[i];
+            s[i] += w[index];
+        }
+
+        // 이번 차례에 깰 계란이 없으면 -> max 값 조정
+        max = Math.max(max, broken);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        int[] s = new int[n];                   // 내구도
+        int[] w = new int[n];                   // 무게
+
+        for (int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            s[i] = Integer.parseInt(st.nextToken());
+            w[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0, 0, s, w);
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }

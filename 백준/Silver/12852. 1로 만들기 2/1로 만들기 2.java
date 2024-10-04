2b48bf44f6700f7e68f15c56ed8483e7495f7b97
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2024-10-04 14:41:40 +0900

    [Silver I] Title: 1로 만들기 2, Time: 120 ms, Memory: 22016 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -1,34 +1,55 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
 
 public class Main {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
 
-        int dp[] = new int[N+1];
-        int trace[] = new int[N+1];
+    static int[] saved;         // 메모이제이션
+    static int[] to;            // 경로 저장, to[i] == i번째 수가 1로 가는 다음 경로
 
-        dp[0] = dp[1] = 0;
-        for(int i=2; i<=N; i++){
-            dp[i] = dp[i-1]+1;
-            trace[i] = i-1;
-            if(i%2==0 && dp[i]>dp[i/2]+1){
-                dp[i] = dp[i/2]+1;
-                trace[i] = i/2;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        saved = new int[n + 1];
+        to = new int[n + 1];
+        saved[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            saved[i] = saved[i - 1] + 1;
+            to[i] = i - 1;
+
+            if (i % 3 == 0 && saved[i] > saved[i / 3] + 1) {
+                saved[i] = saved[i / 3] + 1;
+                to[i] = i / 3;
             }
-            if(i%3==0 && dp[i]>dp[i/3]+1){
-                dp[i] = dp[i/3]+1;
-                trace[i] = i/3;
+            if (i % 2 == 0 && saved[i] > saved[i / 2] + 1) {
+                saved[i] = saved[i / 2] + 1;
+                to[i] = i / 2;
             }
         }
-        System.out.println(dp[N]);
 
         StringBuilder sb = new StringBuilder();
-        while(N>0){
-            sb.append(N+ " ");
-            N = trace[N];
+        int i = n;
+        sb.append(n).append(" ");
+        while (true) {
+            if (i == 1) break;
+
+            sb.append(to[i]).append(" ");
+            i = to[i];
         }
+
+        System.out.println(saved[n]);
         System.out.println(sb);
 
+        br.close();
     }
 }
+
+/**
+ * 실버 1 12852 1로 만들기 2
+ *
+ * 1로 만드는 최소방법의 과정을 어떻게 출력??
+ * -> bottom up 방식으로 saved[], to[] 배열 구성해가기
+ *
+ *
+ */

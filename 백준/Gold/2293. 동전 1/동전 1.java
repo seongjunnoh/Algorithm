fdccbd11364618bc45d579a933f045ff0cfcd249
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2024-10-12 15:59:43 +0900

    [Gold IV] Title: 동전 1, Time: 272 ms, Memory: 18700 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, k;
+    static Integer[] values;
+    static int[][] saved;       // saved[index][remain] : index ~ n-1 까지의 동전으로 remain을 구성하는 방법의 수
+
+    static int dp(int index, int remain) {
+        if (remain == 0) return 1;
+        if (index == n) return 0;           
+        if (saved[index][remain] != -1) return saved[index][remain];
+
+        int maxCount = remain / values[index];          // values[index]의 가능한 최대 개수
+        saved[index][remain] = 0;       // -1 -> 0 으로 update
+        for (int count = maxCount; count >= 0; count--) {
+            saved[index][remain] += dp(index + 1, remain - values[index] * count);
+        }
+
+        return saved[index][remain];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        values = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            values[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(values, Collections.reverseOrder());        // 내림차순 정렬
+
+        saved = new int[n][k+1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(saved[i], -1);          // 초기화 = -1 (아직 게산하지 않은 값을 -1로 초기화)
+        }
+
+        System.out.println(dp(0, k));
+        br.close();
+    }
+}
+
+/**
+ * 골드 4 2293 동전 1
+ *
+ * 사용한 동전의 구성이 달라야 함
+ *
+ * 10 -> 5 2개, 5 1개 2 2개 1 1개, 5 1개 2 1개 1 3개, 2 5개, ,,,
+ * 현재 고려하는 동전 최대한 사용 & 나머지 금액은 이전 메모이제이션 결과 이용
+ *
+ */

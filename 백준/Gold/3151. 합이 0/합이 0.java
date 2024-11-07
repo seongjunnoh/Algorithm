3541f1ecf2490c3258eb4a3cdb213f4bb3d2fac5
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2024-11-07 15:00:33 +0900

    [Gold IV] Title: 합이 0, Time: 340 ms, Memory: 20556 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -14,34 +14,40 @@ public class Main {
 
         Arrays.sort(arr);
 
-        long count = 0;
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                int target = -(arr[i] + arr[j]);
+        long count = 0;         // 10000개 중 3개 고르는 것 -> long type
+        for (int i = 0; i < n; i++) {
+            int fix = arr[i];
+            if (fix > 0) break;
 
-                // target의 lower bound 찾기
-                int l = j + 1;
-                int r = n - 1;
-                while (l <= r) {
-                    int mid = (l + r) / 2;
+            int l = i + 1;
+            int r = n - 1;
 
-                    if (arr[mid] >= target) r = mid - 1;
-                    else l = mid + 1;
+            while (l < r) {
+                // 동일한 arr[l], arr[r] 의 개수
+                int cntL = 1;
+                int cntR = 1;
+
+                int current = fix + arr[l] + arr[r];
+                if (current == 0) {
+                    if (arr[l] == arr[r]) {
+                        count += (r - l + 1) * (r - l) / 2;
+                        break;
+                    }
+
+                    while (l + 1 < r && arr[l] == arr[l + 1]) {
+                        cntL++;
+                        l++;
+                    }
+
+                    while (l < r - 1 && arr[r] == arr[r - 1]) {
+                        cntR++;
+                        r--;
+                    }
+                    count += cntL * cntR;
                 }
-                int lowerBound = l;
 
-                // target의 upper bound 찾기
-                l = j + 1;
-                r = n - 1;
-                while (l <= r) {
-                    int mid = (l + r) / 2;
-
-                    if (arr[mid] <= target) l = mid + 1;
-                    else r = mid - 1;
-                }
-                int upperBound = l;
-
-                count += upperBound - lowerBound;
+                if (current > 0) r--;
+                else l++;
             }
         }
 
@@ -54,7 +60,7 @@ public class Main {
  * 골드 4 3151 합이 0
  *
  * arr 중 3개의 합이 0이 되는 세트를 몇개 만들 수 있는가?
- * -> a + b + c = 0 <=> a + b = -c
- *      => a, b 찾고 (n 제곱) && 이분탐색으로 c 찾기 (log n)
- *      => 이때, a, b, c 모두 중복이 허용되므로 c의 개수는 upper bound - lower bound
+ * -> a + b + c = 0
+ *      => 첫번째 수 고정 (n) && 2, 3 번째 수 투 포인터로 찾기 (n) => n 제곱
+ *
  */

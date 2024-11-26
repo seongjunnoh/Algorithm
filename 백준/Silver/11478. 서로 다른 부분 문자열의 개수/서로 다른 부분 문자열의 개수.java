22e7a585b3c7ba580957565136d3e48e8fd56cab
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2024-11-26 14:01:47 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 692 ms, Memory: 227156 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -3,29 +3,15 @@ import java.util.*;
 
 public class Main {
 
-    static String line;
-    static Set<String> set = new HashSet<>();
-    static boolean[] visit;
-
-    static void cal(int idx, int len) {        // idx : 부분수열 시작 idx, len : 부분수열의 길이
-        if (idx + len > line.length()) return;
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = idx; i < idx + len; i++) {
-            sb.append(line.charAt(i));
-        }
-        set.add(sb.toString());
-        
-        cal(idx + 1, len);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        line = br.readLine();
-        visit = new boolean[line.length()];
+        String line = br.readLine();
 
-        for (int len = 1; len <= line.length(); len++) {
-            cal(0, len);
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < line.length(); i++) {
+            for (int j = i + 1; j <= line.length(); j++) {
+                set.add(line.substring(i, j));
+            }
         }
 
         System.out.println(set.size());
@@ -36,6 +22,6 @@ public class Main {
 /**
  * 실버3 11478번 서로 다른 부분 문자열의 개수
  *
- * 전체 부분 문자열을 재귀함수 로 구하기 & set 으로 중복 제거해서 관리
+ * 전체 부분 문자열을 이중 for loop 로 구하기 & set 으로 중복 제거해서 관리
  *
  */

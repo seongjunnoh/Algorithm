61bc9ce9f2ab9c822291321cd926b17bd3390953
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2024-11-27 17:31:09 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2548 ms, Memory: 363268 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -3,24 +3,6 @@ import java.util.*;
 
 public class Main {
 
-    static Map<Integer, Integer> map;
-
-    static int delete(Queue<Integer> q) {      // q에서 poll & map과 동기화
-        int poll = 0;
-        while (true) {
-            poll = q.poll();
-            Integer count = map.getOrDefault(poll, 0);
-
-            if (count == 0) continue;
-
-            else if (count == 1) map.remove(poll);
-            else map.put(poll, map.get(poll) - 1);
-            break;
-        }
-
-        return poll;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
@@ -28,9 +10,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             int k = Integer.parseInt(br.readLine());
-            PriorityQueue<Integer> minQ = new PriorityQueue<>();
-            PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
-            map = new HashMap<>();
+            TreeMap<Integer, Integer> map = new TreeMap<>();        // TreeMap 생성
 
             for (int j = 0; j < k; j++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
@@ -38,27 +18,26 @@ public class Main {
                 int x = Integer.parseInt(st.nextToken());
 
                 if (oper == 'I') {
-                    minQ.add(x);
-                    maxQ.add(x);
                     map.put(x, map.getOrDefault(x, 0) + 1);
                 } else {
                     if (map.size() == 0) continue;
 
-                    if (x == -1) delete(minQ);
-                    else delete(maxQ);
+                    if (x == -1) {      // 최소값 제거
+                        Integer minKey = map.firstKey();
+                        map.put(minKey, map.get(minKey) - 1);
+                        if (map.get(minKey) == 0) map.remove(minKey);
+                    } else {        // 최대값 제거
+                        Integer maxKey = map.lastKey();
+                        map.put(maxKey, map.get(maxKey) - 1);
+                        if (map.get(maxKey) == 0) map.remove(maxKey);
+                    }
                 }
             }
 
             if (map.size() == 0) {
                 sb.append("EMPTY").append("\n");
             } else {
-                int max = delete(maxQ);
-                if (map.size() == 0) {      // map에 숫자가 1개만 있었는 경우
-                    sb.append(max).append(" ").append(max).append("\n");
-                } else {
-                    int min = delete(minQ);
-                    sb.append(max).append(" ").append(min).append("\n");
-                }
+                sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
             }
         }
 
@@ -78,13 +57,6 @@ public class Main {
  *
  * ???
  * ----------------------------------------------
- * 우선순위 큐 2개 사용 & 이 2개를 동기화 시켜줘야 함
- * -> 한쪽에서 poll 하면, 다른 쪽도 poll한 값을 remove 해줘야함
- * -> 이때 그냥 remove() 메서드 사용하면 마찬가지로 O(n) 이므로 시간초과
- *      => 입력받은 숫자들의 정보를 저장하는 map 도입
- *         큐에서 뽑은 숫자가 map에 없다
- *         == 다른 쪽 큐에서 이미 뽑아서 map에 반영하였다
- *         == 다음 숫자를 뽑아야 함
- *         => 반복
+ * TreeMap 자료구조 이용하자
  *
  */

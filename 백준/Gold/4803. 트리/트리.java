fab02ebb0fecf23b7e327f68ca03dac6467bc62c
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2024-12-23 18:59:32 +0900

    [Gold IV] Title: 트리, Time: 480 ms, Memory: 63340 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.java
+++ 백준/Gold/4803. 트리/트리.java
@@ -3,34 +3,32 @@ import java.util.*;
 
 public class Main {
 
-    static Map<Integer, Map<Integer, Boolean>> graph;       // graph.get(i) : i 노드와 연결된 노드들의 상태를 나타내는 map (-> key : 연결된 노드의 번호, val : bfs 대상 여부)
+    static List<Integer>[] graph;
     static boolean[] visit;
     static int result;
 
     static void bfs(int start) {
         Queue<Integer> q = new LinkedList<>();
+        int n = 0;
+        int e = 0;      // node, edge 개수
+
         q.add(start);
         visit[start] = true;
-
         while (!q.isEmpty()) {
             int poll = q.poll();
+            n++;
 
-            Map<Integer, Boolean> nears = graph.get(poll);
-            for (int near : nears.keySet()) {
-                if (nears.get(near)) {          // bfs 대상이 되는 poll -> near 에 대해서만
-                    graph.get(near).put(poll, false);           // near -> poll 로 가는 에지는 bfs 대상 아님
+            for (int near : graph[poll]) {
+                e++;
 
-                    if (nears.get(near) && !visit[near]) {      // poll -> near가 bfs 대상이고, near가 방문하지 않은 노드인 경우
-                        visit[near] = true;
-                        q.add(near);
-                    } else {
-                        return;     // 현재 연결요소는 사이클이다
-                    }
+                if (!visit[near]) {
+                    visit[near] = true;
+                    q.add(near);
                 }
             }
         }
 
-        result++;       // 연결요소 개수 하나 추가
+        if (2 * (n - 1) == e) result++;     // tree 하나 추가
     }
 
     public static void main(String[] args) throws IOException {
@@ -46,17 +44,17 @@ public class Main {
 
             if (n == 0 && m == 0) break;
 
-            graph = new HashMap<>();
+            graph = new ArrayList[n + 1];
             for (int i = 1; i <= n; i++) {
-                graph.put(i, new HashMap<>());
+                graph[i] = new ArrayList<>();
             }
 
             for (int i = 0; i < m; i++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                graph.get(x).put(y, true);
-                graph.get(y).put(x, true);
+                graph[x].add(y);
+                graph[y].add(x);
             }
 
             // bfs로 연결요소 개수 찾기
@@ -84,4 +82,7 @@ public class Main {
  * bfs로 연결요소 개수 찾기
  *
  * 1->2, 2->1 : 1->2 고려할 경우, 2->1 에지를 false로 변경해야함 & bfs는 true인 에지에 대해서만 수행
+ * ==============================
+ * 방법2 -> node - 1 = edge 인 경우 해당 연결요소를 트리라고 할 수 있음
+ * 방법3 -> union find 로 union 의 개수 찾기
  */

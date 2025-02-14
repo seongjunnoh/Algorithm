22da200187a1e53e6c2caeb750af3a41783637de
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2025-02-14 15:53:04 +0900

    [Gold III] Title: 파일 합치기, Time: 752 ms, Memory: 36544 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Solution_11066 {
+class Solution_11066_1 {
 
     int[] sum;
     long[][] dp;
@@ -24,35 +24,32 @@ class Solution_11066 {
             }
 
             dp = new long[k + 1][k + 1];
-            for (int from = 1; from <= k; from++) {         // dp 초기화
-                for (int to = from; to <= k; to++) {
-                    if (from == to) dp[from][to] = 0;
-                    else if (from + 1 == to) dp[from][to] = sum[to] - sum[from - 1];
-                    else dp[from][to] = INF;
+            for (int num = 1; num <= k; num++) {         // dp 초기화
+                dp[num][num] = 0;
+            }
+
+            for (int len = 2; len <= k; len++) {
+                for (int from = 1; from <= k - len + 1; from++) {
+                    int to = from + len - 1;
+                    dp[from][to] = INF;
+
+                    for (int mid = from; mid < to; mid++) {
+                        dp[from][to] = Math.min(dp[from][to], dp[from][mid] + dp[mid + 1][to] + sum[to] - sum[from - 1]);
+                    }
                 }
             }
 
-            sb.append(play(1, k)).append("\n");
+            sb.append(dp[1][k]).append("\n");
         }
 
         System.out.println(sb);
         br.close();
     }
-
-    long play(int from, int to) {
-        if (dp[from][to] != INF) return dp[from][to];
-
-        for (int mid = from; mid < to; mid++) {
-            dp[from][to] = Math.min(dp[from][to], play(from, mid) + play(mid + 1, to) + sum[to] - sum[from - 1]);
-        }
-
-        return dp[from][to];
-    }
 }
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Solution_11066 s = new Solution_11066();
+        Solution_11066_1 s = new Solution_11066_1();
         s.solution();
     }
 }
@@ -62,5 +59,6 @@ public class Main {
  *
  * dp[i][j] : i부터 j파일 까지 합칠 때 필요한 최소비용
  * -> dp[1][k] = dp[1][1] + dp[2][k] + (1~k까지 누적합), dp[1][2] + dp[3][k] + (1~k까지 누적합), ,,, 중 최소값
- *
+ * -------------------------------
+ * bottom up 방식
  */

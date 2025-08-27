b4b3c5f99dc0dde4db071c4112851b01a5173d16
SeongjunNoh <145101180+seongjunnoh@users.noreply.github.com>
2025-08-27 12:53:42 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 324 ms, Memory: 27240 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -30,25 +30,20 @@ class Solution {
             parent[i] = i;  // 초기화
         }   
         
+        int result = 0; // 집합의 개수
         for (int x=0; x<n; x++) {   // 유니온 파인드
             for (int y=0; y<m; y++) {
                 int curD = dir[x][y];
                 int nX = x + pos[curD][0];
                 int nY = y + pos[curD][1];
                 
-                union(toIdx(x, y), toIdx(nX, nY));
+                if (!union(toIdx(x, y), toIdx(nX, nY))) {
+                    result++;   // 이미 같은 사이클인 경우
+                }
             }
         }   
         
-        HashSet<Integer> set = new HashSet<>();
-        for (int x=0; x<n; x++) {   // 유니온 파인드
-            for (int y=0; y<m; y++) {
-                // 현재 지점의 루트를 set에 add
-                set.add(find(toIdx(x, y)));
-            }
-        }   
-        
-        System.out.println(set.size());
+        System.out.println(result);
         br.close();
     }
     
@@ -56,12 +51,17 @@ class Solution {
         return x * m + y;   
     }
     
-    void union(int prev, int next) {    
+    boolean union(int prev, int next) {    
         int rP = find(prev);
         int rN = find(next);
         
         // next의 루트를 prev의 루트로 업데이트
-        if (rP != rN) parent[rN] = rP;
+        if (rP != rN) {
+            parent[rN] = rP;
+            return true;
+        }
+        
+        return false;   // union 하지 못함 -> 이미 prev, next는 같은 집합
     }
     
     int find(int node) {
@@ -90,5 +90,4 @@ public class Main
  * map의 각 지점은 무조건 outgoing dir 이 1개이므로 사이클의 집합으로 구성된다
  * -> 유니온 파인드로 얻은 집합 1개당 세이프존 1개인 것이 최소이다
  * 
- * find 시에 경로압축 없으니 시간초과 발생
  */
